6bf896a8135fdcd67ab099a8bac49f6697b18cc2
Omar <omar.abuatieh01@gmail.com>
2024-12-05 01:57:51 +0100

    Fourth commit, table is sorted.

--- project/src/project/Main.java
+++ project/src/project/Main.java
@@ -1,24 +1,30 @@
 package project;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		int[] numbers = new int[10];
-		
+
 		Random random = new Random();
-		
-		for(int i=0; i<numbers.length; i++) {
+
+		for (int i = 0; i < numbers.length; i++) {
 			numbers[i] = random.nextInt(101);
 		}
-		
+
 		System.out.println("Table elements:");
 		for (int number : numbers) {
 			System.out.println(number);
 		}
 
+		Arrays.sort(numbers);
+		System.out.println("Table elements after sorting:");
+		for (int number : numbers) {
+			System.out.println(number);
+		}
 	}
 
 }
